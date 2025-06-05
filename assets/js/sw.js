55f339ff1252ef180ef09ab02bcc024c71f212cf
Christopher Tejena <92233259+ByChriis15@users.noreply.github.com>
2025-06-04 22:11:09 -0500

    Add files via upload

---
+++ assets/js/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'mariley-cache-v1';
+const urlsToCache = [
+  '/',
+  '/pages/budget.html',
+  '/assets/css/styles.css',
+  '/assets/js/main.js',
+  '/assets/img/icon-192.png',
+  '/assets/img/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
